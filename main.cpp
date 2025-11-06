a9ea19a013b32b8b64f628670abd680a4db67b91
Just1nWu <1295360720@qq.com>
2025-11-06 12:00:35 +0800

    func, chore: 添加了将分裂的正则表达式转为后缀表达式的函数.
    - 添加了将分裂的正则表达式转为后缀表达式的函数.
    - 添加了一些输出函数以便调试.
    - 修改了一些不规范的函数,变量命名.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ string input_str;
 class NFA {
 private:
     string regexp;
-    vector<char> split_regexp;
+    vector<char> splited_regexp;
+    vector<char> postfix_regexp;
 
     // 判断是否需要在两个字符之间插入连接符
     bool need_split(char ch1, char ch2){
@@ -20,11 +21,23 @@ private:
 public:
     NFA(const string &input_str){
         regexp = input_str;
-        split_regexp.clear();
+        splited_regexp.clear();
     }
 
-    void print_split_regexp(const string &regexp){
-        for (auto ch : split_regexp)
+    void print_regexp(){
+        cout << "Input Expression: " << regexp << endl;
+    }
+
+    void print_splited_regexp(){
+        cout << "Splited Expression: ";
+        for (auto ch : splited_regexp)
+            cout << ch;
+        cout << endl;
+    }
+
+    void print_postfix_regexp(){
+        cout << "Postfix Expression: ";
+        for (auto ch : postfix_regexp)
             cout << ch;
         cout << endl;
     }
@@ -34,12 +47,56 @@ public:
         char nowc, nextc;
         for (int i = 0; i < regexp.size(); i++){
             nowc = regexp[i];
-            split_regexp.push_back(nowc);
+            splited_regexp.push_back(nowc);
             if (i + 1 < regexp.size()){
                 nextc = regexp[i + 1];
                 if (need_split(nowc, nextc))
-                    split_regexp.push_back('.');
+                    splited_regexp.push_back('.');
             }
+        }
+    }
+
+    int op_priority(char op){
+        switch (op){
+            case '*' : return 3;
+            case '.' : return 2;
+            case '|' : return 1;
+            default  : return 0; // 括号优先级最低
+        }
+    }
+    
+    // 由正则表达式构建后缀表达式
+    void build_postfix_regexp(){
+        stack<char> op_stack;
+        postfix_regexp.clear();
+
+        for (auto ch : splited_regexp)
+            if (isalpha(ch)){
+                postfix_regexp.push_back(ch);
+            }
+            else if (ch == '('){
+                op_stack.push(ch);
+            }
+            else if (ch == ')'){ 
+                // 此时括号内操作符的优先级别一定是递增的，从往前算
+                while (op_stack.top() != '('){
+                    postfix_regexp.push_back(op_stack.top());
+                    op_stack.pop();
+                }
+                op_stack.pop();
+            }
+            else { // * 或 ｜
+                while (!op_stack.empty() && op_priority(op_stack.top()) >= op_priority(ch)){
+                    // 保证栈内优先级递增，即需要从后往前计算
+                    postfix_regexp.push_back(op_stack.top());
+                    op_stack.pop();
+                }
+                op_stack.push(ch);
+            }
+        // 处理残余操作符
+        while (!op_stack.empty()){
+            postfix_regexp.push_back(op_stack.top());
+            op_stack.pop();
         }
     }
 };
@@ -49,8 +106,16 @@ int main(){
     freopen("out.txt", "w", stdout);
     while (cin >> input_str){
         NFA nfa(input_str);
+
+        nfa.print_regexp();
+
         nfa.split_input_regexp();
-        nfa.print_split_regexp(input_str);
+        nfa.print_splited_regexp();
+
+        nfa.build_postfix_regexp();
+        nfa.print_postfix_regexp();
+
+        cout << endl;
     }
     return 0;
 }
