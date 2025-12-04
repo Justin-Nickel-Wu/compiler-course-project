45f4d0d96311e56a7489f5f8939c007cb8ba73bf
Just1nWu <1295360720@qq.com>
2025-12-04 11:22:07 +0800

    refactor: 完善了main.cpp更方便词法单元测试。
    - 新增了一些格式化输出函数。

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,94 @@
 #include "flex_config.hpp"
 using namespace std;
 
-int main() {
-    printf("main sees yylval at %p\n", (void *)&yylval);
-    yyFlexLexer lexer;
+template <typename T>
+string red(const T &input) {
+    ostringstream oss;
+    oss << "\033[31m" << input << "\033[0m";
+    return oss.str();
+}
+
+template <typename T>
+string green(const T &input) {
+    std::ostringstream oss;
+    oss << "\033[32m" << input << "\033[0m";
+    return oss.str();
+}
+
+void Err(char type, int line, string desc = "") {
+    cout << red("Error type " + string(1, type) + " at Line " + to_string(line));
+    if (!desc.empty()) {
+        cout << red(": " + desc) << endl;
+    } else {
+        cout << red(".") << endl;
+    }
+}
+
+void Ok(string desc) {
+    cout << green("OK: " + desc) << endl;
+}
+
+void part1_test() {
+    struct info {
+        int token, line;
+        string text;
+        TokenValue val;
+        info(int t, int l, string txt, TokenValue v) : token(t), line(l), text(txt), val(v) {}
+    };
+
     ifstream fin("input.txt");
     if (!fin.is_open()) {
-        cerr << "Failed to open input.txt" << endl;
-        return 1;
+        cout << red("Failed to open input.txt\n") << endl;
+        return;
     }
+    cout << green("File opened successfully.") << endl;
+
+    yyFlexLexer lexer;
+    int token;
+    bool something_wrong = false;
+    vector<info> tokens;
+
     lexer.switch_streams(&fin, nullptr);
-
-    int tok;
-    while ((tok = lexer.yylex()) != 0) {
-        cout << "Line " << lexer.lineno() << " " << lexer.YYText() << " -> " << tokenName(tok) << endl;
-        cout << "   " << "fval: " << yylval.fval << ", ival: " << yylval.ival << ", ident: " << (yylval.ident == NULL ? "NULL" : yylval.ident) << endl;
+    while ((token = lexer.yylex()) != 0) {
+        switch (token) {
+            case BAD_OCT: {
+                Err('A', lexer.lineno(), "Invalid octal constant: " + string(lexer.YYText()));
+                something_wrong = true;
+                break;
+            }
+            case BAD_HEX: {
+                Err('A', lexer.lineno(), "Invalid hex constant: " + string(lexer.YYText()));
+                something_wrong = true;
+                break;
+            }
+            case BAD_FLOAT: {
+                Err('A', lexer.lineno(), "Invalid float constant: " + string(lexer.YYText()));
+                something_wrong = true;
+                break;
+            }
+            case BAD_IDENT: {
+                Err('A', lexer.lineno(), "Invalid identifier: " + string(lexer.YYText()));
+                something_wrong = true;
+                break;
+            }
+            default: {
+                tokens.push_back(info(token, lexer.lineno(), lexer.YYText(), yylval));
+            }
+        }
     }
+    fin.close();
+    if (!something_wrong) {
+        Ok("No errors found.");
+        for (int i = 0; i < tokens.size(); i++) {
+            if (i == 0 || tokens[i - 1].line != tokens[i].line)
+                cout << "Line " << tokens[i].line << ":" << endl;
+            cout << "   ";
+            cout << tokens[i].text << " -> " << tokenName(tokens[i].token) << endl;
+        }
+    }
+}
 
+int main() {
+    part1_test();
     return 0;
 }
